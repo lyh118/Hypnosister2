b5e0bff2caf59994acdc28cf835697282cbd157c
N3962 <n3962@N3962ui-Mac-mini.local>
2015-04-02 15:32:05 +0900

    Initial Commit

---
+++ Hypnosister/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Hypnosister
+//
+//  Created by N3962 on 2015. 4. 2..
+//  Copyright (c) 2015년 BigNerdRanch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Hypnosister/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Hypnosister
+//
+//  Created by N3962 on 2015. 4. 2..
+//  Copyright (c) 2015년 BigNerdRanch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
